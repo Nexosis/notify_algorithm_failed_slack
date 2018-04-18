7a8a866e63213d9719f3ce20453be77893f5853b
Craig Vermeer <craig@vermeers.net>
2018-04-18 13:28:54 -0400

    Forward on to slack webhook

--- NotifyAlgorithmFailed/index.js
+++ NotifyAlgorithmFailed/index.js
@@ -1,17 +1,27 @@
-module.exports = function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+var request = require('request')
 
-    if (req.query.name || (req.body && req.body.name)) {
+module.exports = function (context, req) {
+
+    if (req.body) {
         context.res = {
             // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
         };
+        var host = req.body.request.host;
+        var jobId = req.body.target.repository;
+        var tag = req.body.taret.tag;
+
+        var notificationUrl = process.env["NotificationUrl"]
+
+        var msg = {
+            text : "Algorithm Dump generated at " + host + "/" + jobId + ":" + tag
+        }
+
+        request.post(notificationUrl, {json: msg}, function(error, response, body) {
+            context.done();
+        });
     }
     else {
-        context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
-        };
+        context.done();
     }
-    context.done();
+    
 };
